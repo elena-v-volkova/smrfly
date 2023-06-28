af1eb5edb8843559de152d40dfc82353556b1b5c
unknown <interferention.int@gmail.com>
2023-06-28 17:44:38 +0300

    add styles and media in media block

---
+++ js/script.js
@@ -0,0 +1,44 @@
+var swiper = new Swiper(".mySwiper", {
+    spaceBetween:  0,
+    autoplay: {
+        delay: 2000,
+      },
+
+  });
+
+  var swiper = new Swiper(".mySwiper2", {
+    spaceBetween:  0,
+    direction: 'vertical',
+    autoplay: {
+        delay: 6000,
+      },
+
+  });
+
+  var swiper = new Swiper(".mySwiper3", {
+    spaceBetween:  0,
+    autoplay: {
+        delay: 5000,
+      },
+
+  });
+
+  var swiper = new Swiper(".mySwiper4", {
+    spaceBetween:  0,
+    direction: 'vertical',
+    autoplay: {
+        delay: 3000,
+      },
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+  });
+
+  var swiper = new Swiper(".mySwiper5", {
+    spaceBetween:  0,
+    autoplay: {
+        delay: 7000,
+      },
+
+  });
